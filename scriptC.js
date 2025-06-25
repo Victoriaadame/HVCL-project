a527924beacc7532c52281fbe1fe99bd13a2c491
Carlos Martinez <carlosimportant@gmail.com>
2025-06-25 15:24:59 -0700

    new button message

--- scriptC.js
+++ scriptC.js
@@ -1,8 +1,11 @@
 // If your button has an ID:
-const myButton = document.getElementById('myButton'); 
+const myButton = document.getElementById('myButton');
+
+console.log(myButton);
 
 myButton.addEventListener('click', function() {
   // Code to be executed when the button is clicked
-  console.log('Button was clicked!');
+    alert('Congrats You Learned About CSU Chico!');
+  
   // You can call other functions, modify elements, send data, etc.
 });
