44d0f5a6426aeb71377feae8b6f4674c8f3b604b
Carlos Martinez <carlosimportant@gmail.com>
2025-06-25 15:20:38 -0700

    new button

--- scriptC.js
+++ scriptC.js
@@ -1,13 +1,5 @@
 // If your button has an ID:
-const myButton = document.getElementById('myButtonId'); 
-
-// If your button has a class:
-const myButton = document.querySelector('.myButtonClass'); 
-// (querySelector selects the first element with that class)
-
-// If you want to select the first button element on the page:
-const myButton = document.querySelector('button'); 
-
+const myButton = document.getElementById('myButton'); 
 
 myButton.addEventListener('click', function() {
   // Code to be executed when the button is clicked
